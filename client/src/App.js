204ac0f41f5f158af395fc16065050580b7ca7d4
Giuxpro <albertogiusepp@gmail.com>
2022-05-26 01:16:58 -0300

    front started

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Landing from "../src/components/landing/Landing";
+import Home from "../src/components/home/Home";
+import Detail from "../src/components/detail/Detail";
+import PokemonCreate from "../src/components/form/PokemonCreate"
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Landing/>}/>
+          <Route exact path='home' element={<Home/>}/>
+          <Route exact path='/pokemons/:id' element={<Detail/>}/>
+          <Route exact path='/pokemos' element={<PokemonCreate/>}/>
+          <Route path='*' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const rootReducer = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default rootReducer
